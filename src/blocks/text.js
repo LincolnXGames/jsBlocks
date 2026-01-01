31920316d563ecf0b99fdb115f9d97db76697e71
LincolnX <notlincolnx@gmail.com>
2026-01-01 12:28:23 -0600

    Remove import statement for Blockly

--- src/blocks/text.js
+++ src/blocks/text.js
@@ -4,7 +4,6 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import * as Blockly from 'blockly/core';
 
 // Create a custom block called 'add_text' that adds
 // text to the output div on the sample app.
