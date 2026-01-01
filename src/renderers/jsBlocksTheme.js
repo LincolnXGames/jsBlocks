75a4a9475e3f9fcf93630652f01f910ee294e1f8
LincolnX <notlincolnx@gmail.com>
2026-01-01 13:39:59 -0600

    Create jsBlocksTheme.js

---
+++ src/renderers/jsBlocksTheme.js
@@ -0,0 +1,21 @@
+export const theme = Blockly.Theme.defineTheme('jsBlocks_theme', {
+   'base': Blockly.Themes.Zelos,
+   'blockStyles': {
+      'loop_blocks': {
+         'colourPrimary': '#ffab19',
+         'colourSecondary': '#ec9c13',
+         'colourTertiary': '#cf8b17'
+      },
+   },
+   'categoryStyles': {
+      'loop_category': {
+         'colour': '#ffab19'
+      },
+   },
+   'fontStyle': {
+      'family': '"Helvetica Neue", Helvetica, Arial, sans-serif',
+      'weight': 500,
+      'size': 12
+    },
+   'startHats': true
+});
