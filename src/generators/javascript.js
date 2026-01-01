ccc9c4336f22bb75c72952e00c9ce90237c140a5
LincolnX <notlincolnx@gmail.com>
2026-01-01 14:14:09 -0600

    Update javascript.js

--- src/generators/javascript.js
+++ src/generators/javascript.js
@@ -11,6 +11,17 @@
 const jsGenerator = Blockly.JavaScript
 export const forBlock = Object.create(null);
 
+forBlock['custom_ternary'] = function (block, generator) {
+  const value_if = generator.valueToCode(block, 'IF', jsGenerator.ORDER_CONDITIONAL) || 'false';
+  const value_then = generator.valueToCode(block, 'THEN', jsGenerator.ORDER_CONDITIONAL) || 'null';
+  const value_else = generator.valueToCode(block, 'ELSE', jsGenerator.ORDER_CONDITIONAL) || 'null';
+
+  // TODO: Assemble javascript into the code variable.
+  const code = value_if + ' ? ' + value_then + ' : ' +  value_else;
+  // TODO: Change Order.NONE to the correct operator precedence strength
+  return [code, jsGenerator.ORDER_CONDITIONAL];
+};
+
 forBlock['add_text'] = function (block, generator) {
   const text = generator.valueToCode(block, 'TEXT', jsGenerator.ORDER_ATOMIC) || "''";
   const addText = generator.provideFunction_(
@@ -29,13 +40,15 @@ forBlock['add_text'] = function (block, generator) {
   return code;
 };
 
-forBlock['custom_ternary'] = function (block, generator) {
-  const value_if = generator.valueToCode(block, 'IF', jsGenerator.ORDER_CONDITIONAL) || 'false';
-  const value_then = generator.valueToCode(block, 'THEN', jsGenerator.ORDER_CONDITIONAL) || 'null';
-  const value_else = generator.valueToCode(block, 'ELSE', jsGenerator.ORDER_CONDITIONAL) || 'null';
-
-  // TODO: Assemble javascript into the code variable.
-  const code = value_if + ' ? ' + value_then + ' : ' +  value_else;
-  // TODO: Change Order.NONE to the correct operator precedence strength
-  return [code, jsGenerator.ORDER_CONDITIONAL];
+forBlock['text_input'] = function (block, generator) {
+  const text_text = block.getFieldValue('TEXT');
+  const quote = (string) => {
+    string = string
+      .replace(/\\/g, '\\\\')
+      .replace(/\n/g, '\\\n')
+      .replace(/'/g, "\\'");
+    return "'" + string + "'";
+  }
+  const code = quote(text_text);
+  return [code, jsGenerator.ORDER_ATOMIC];
 };
