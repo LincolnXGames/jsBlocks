59ef18f779797bf5c5392b69a14ae58a76011598
LincolnX <notlincolnx@gmail.com>
2026-01-01 14:14:05 -0600

    Update text.js

--- src/blocks/text.js
+++ src/blocks/text.js
@@ -25,10 +25,31 @@ const addText = {
   tooltip: '',
   helpUrl: '',
 };
+const textInput = {
+  "type": "text_input",
+  "tooltip": "",
+  "helpUrl": "",
+  "message0": "%1 %2",
+  "args0": [
+    {
+      "type": "field_input",
+      "name": "TEXT",
+      "text": ""
+    },
+    {
+      "type": "input_dummy",
+      "name": "NAME"
+    }
+  ],
+  "output": "String",
+  "style": "text_blocks",
+  "extensions": ['parent_tooltip_when_inline'],
+};
 
 // Create the block definitions for the JSON-only blocks.
 // This does not register their definitions with Blockly.
 // This file has no side effects!
 export const blocks = Blockly.common.createBlockDefinitionsFromJsonArray([
   addText,
+  textInput,
 ]);
