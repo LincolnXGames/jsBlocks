253889d90e5f3a667c31dc1488b561a4570faadd
LincolnX <notlincolnx@gmail.com>
2026-01-01 13:53:44 -0600

    Update toolbox.js

--- src/toolbox.js
+++ src/toolbox.js
@@ -1,60 +1,15 @@
-/**
- * @license
- * Copyright 2023 Google LLC
- * SPDX-License-Identifier: Apache-2.0
- */
-
-/*
-This toolbox contains nearly every single built-in block that Blockly offers,
-in addition to the custom block 'add_text' this sample app adds.
-You probably don't need every single block, and should consider either rewriting
-your toolbox from scratch, or carefully choosing whether you need each block
-listed here.
-*/
-
 export const toolbox = {
   kind: 'categoryToolbox',
   contents: [
     {
       kind: 'category',
-      name: 'Logic',
-      categorystyle: 'logic_category',
+      name: 'Controls',
+      categorystyle: 'loop_category',
       contents: [
         {
           kind: 'block',
           type: 'controls_if',
         },
-        {
-          kind: 'block',
-          type: 'logic_compare',
-        },
-        {
-          kind: 'block',
-          type: 'logic_operation',
-        },
-        {
-          kind: 'block',
-          type: 'logic_negate',
-        },
-        {
-          kind: 'block',
-          type: 'logic_boolean',
-        },
-        {
-          kind: 'block',
-          type: 'logic_null',
-        },
-        {
-          kind: 'block',
-          type: 'logic_ternary',
-        },
-      ],
-    },
-    {
-      kind: 'category',
-      name: 'Loops',
-      categorystyle: 'loop_category',
-      contents: [
         {
           kind: 'block',
           type: 'controls_repeat_ext',
@@ -111,11 +66,203 @@ export const toolbox = {
           kind: 'block',
           type: 'controls_flow_statements',
         },
+        {
+          kind: 'block',
+          type: 'custom_ternary',
+        },
       ],
     },
     {
       kind: 'category',
-      name: 'Math',
+      name: 'Text',
+      categorystyle: 'text_category',
+      contents: [
+        {
+          kind: 'block',
+          type: 'text',
+        },
+        {
+          kind: 'block',
+          type: 'text_join',
+        },
+        {
+          kind: 'block',
+          type: 'text_append',
+          inputs: {
+            TEXT: {
+              shadow: {
+                type: 'text',
+                fields: {
+                  TEXT: '',
+                },
+              },
+            },
+          },
+        },
+        {
+          kind: 'block',
+          type: 'text_length',
+          inputs: {
+            VALUE: {
+              shadow: {
+                type: 'text',
+                fields: {
+                  TEXT: 'abc',
+                },
+              },
+            },
+          },
+        },
+        {
+          kind: 'block',
+          type: 'text_isEmpty',
+          inputs: {
+            VALUE: {
+              shadow: {
+                type: 'text',
+                fields: {
+                  TEXT: '',
+                },
+              },
+            },
+          },
+        },
+        {
+          kind: 'block',
+          type: 'text_indexOf',
+          inputs: {
+            VALUE: {
+              block: {
+                type: 'variables_get',
+              },
+            },
+            FIND: {
+              shadow: {
+                type: 'text',
+                fields: {
+                  TEXT: 'abc',
+                },
+              },
+            },
+          },
+        },
+        {
+          kind: 'block',
+          type: 'text_charAt',
+          inputs: {
+            VALUE: {
+              block: {
+                type: 'variables_get',
+              },
+            },
+          },
+        },
+        {
+          kind: 'block',
+          type: 'text_getSubstring',
+          inputs: {
+            STRING: {
+              block: {
+                type: 'variables_get',
+              },
+            },
+          },
+        },
+        {
+          kind: 'block',
+          type: 'text_changeCase',
+          inputs: {
+            TEXT: {
+              shadow: {
+                type: 'text',
+                fields: {
+                  TEXT: 'abc',
+                },
+              },
+            },
+          },
+        },
+        {
+          kind: 'block',
+          type: 'text_trim',
+          inputs: {
+            TEXT: {
+              shadow: {
+                type: 'text',
+                fields: {
+                  TEXT: 'abc',
+                },
+              },
+            },
+          },
+        },
+        {
+          kind: 'block',
+          type: 'text_count',
+          inputs: {
+            SUB: {
+              shadow: {
+                type: 'text',
+              },
+            },
+            TEXT: {
+              shadow: {
+                type: 'text',
+              },
+            },
+          },
+        },
+        {
+          kind: 'block',
+          type: 'text_replace',
+          inputs: {
+            FROM: {
+              shadow: {
+                type: 'text',
+              },
+            },
+            TO: {
+              shadow: {
+                type: 'text',
+              },
+            },
+            TEXT: {
+              shadow: {
+                type: 'text',
+              },
+            },
+          },
+        },
+        {
+          kind: 'block',
+          type: 'text_reverse',
+          inputs: {
+            TEXT: {
+              shadow: {
+                type: 'text',
+              },
+            },
+          },
+        },
+        {
+          kind: 'block',
+          type: 'add_text',
+          inputs: {
+            TEXT: {
+              shadow: {
+                type: 'text',
+                fields: {
+                  TEXT: 'abc',
+                },
+              },
+            },
+          },
+        },
+      ],
+    },
+    {
+      kind: 'category',
+      name: 'Operators',
       categorystyle: 'math_category',
       contents: [
         {
@@ -317,193 +464,29 @@ export const toolbox = {
             },
           },
         },
-      ],
-    },
-    {
-      kind: 'category',
-      name: 'Text',
-      categorystyle: 'text_category',
-      contents: [
         {
           kind: 'block',
-          type: 'text',
+          type: 'logic_compare',
         },
         {
           kind: 'block',
-          type: 'text_join',
+          type: 'logic_operation',
         },
         {
           kind: 'block',
-          type: 'text_append',
-          inputs: {
-            TEXT: {
-              shadow: {
-                type: 'text',
-                fields: {
-                  TEXT: '',
-                },
-              },
-            },
-          },
+          type: 'logic_negate',
         },
         {
           kind: 'block',
-          type: 'text_length',
-          inputs: {
-            VALUE: {
-              shadow: {
-                type: 'text',
-                fields: {
-                  TEXT: 'abc',
-                },
-              },
-            },
-          },
+          type: 'logic_boolean',
         },
         {
           kind: 'block',
-          type: 'text_isEmpty',
-          inputs: {
-            VALUE: {
-              shadow: {
-                type: 'text',
-                fields: {
-                  TEXT: '',
-                },
-              },
-            },
-          },
+          type: 'logic_null',
         },
         {
           kind: 'block',
-          type: 'text_indexOf',
-          inputs: {
-            VALUE: {
-              block: {
-                type: 'variables_get',
-              },
-            },
-            FIND: {
-              shadow: {
-                type: 'text',
-                fields: {
-                  TEXT: 'abc',
-                },
-              },
-            },
-          },
-        },
-        {
-          kind: 'block',
-          type: 'text_charAt',
-          inputs: {
-            VALUE: {
-              block: {
-                type: 'variables_get',
-              },
-            },
-          },
-        },
-        {
-          kind: 'block',
-          type: 'text_getSubstring',
-          inputs: {
-            STRING: {
-              block: {
-                type: 'variables_get',
-              },
-            },
-          },
-        },
-        {
-          kind: 'block',
-          type: 'text_changeCase',
-          inputs: {
-            TEXT: {
-              shadow: {
-                type: 'text',
-                fields: {
-                  TEXT: 'abc',
-                },
-              },
-            },
-          },
-        },
-        {
-          kind: 'block',
-          type: 'text_trim',
-          inputs: {
-            TEXT: {
-              shadow: {
-                type: 'text',
-                fields: {
-                  TEXT: 'abc',
-                },
-              },
-            },
-          },
-        },
-        {
-          kind: 'block',
-          type: 'text_count',
-          inputs: {
-            SUB: {
-              shadow: {
-                type: 'text',
-              },
-            },
-            TEXT: {
-              shadow: {
-                type: 'text',
-              },
-            },
-          },
-        },
-        {
-          kind: 'block',
-          type: 'text_replace',
-          inputs: {
-            FROM: {
-              shadow: {
-                type: 'text',
-              },
-            },
-            TO: {
-              shadow: {
-                type: 'text',
-              },
-            },
-            TEXT: {
-              shadow: {
-                type: 'text',
-              },
-            },
-          },
-        },
-        {
-          kind: 'block',
-          type: 'text_reverse',
-          inputs: {
-            TEXT: {
-              shadow: {
-                type: 'text',
-              },
-            },
-          },
-        },
-        {
-          kind: 'block',
-          type: 'add_text',
-          inputs: {
-            TEXT: {
-              shadow: {
-                type: 'text',
-                fields: {
-                  TEXT: 'abc',
-                },
-              },
-            },
-          },
+          type: 'logic_ternary',
         },
       ],
     },
