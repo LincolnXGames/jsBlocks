f657fc7eb1d6db1f52e2584f80629376915d12c8
LincolnX <notlincolnx@gmail.com>
2026-01-01 22:23:34 -0600

    Update lists.js

--- src/blocks/lists.js
+++ src/blocks/lists.js
@@ -17,8 +17,30 @@ const listsContains = {
   "output": "Boolean",
   "style": "list_blocks",
   "inputsInline": true,
+};
+const listsNumbers = {
+  "type": "lists_numbers",
+  "tooltip": "Creates a list of numbers ranging from the 'from' value to the 'to' value. Reverses order if 'from' is greater than 'to'.",
+  "helpUrl": "",
+  "message0": "list of numbers from %1 to %2",
+  "args0": [
+    {
+      "type": "input_value",
+      "name": "FROM",
+      "check": "Number"
+    },
+    {
+      "type": "input_value",
+      "name": "TO",
+      "check": "Number"
+    }
+  ],
+  "output": "Array",
+  "style": "list_blocks",
+  "inputsInline": true,
 }
 
 export const listsBlocks = Blockly.common.createBlockDefinitionsFromJsonArray([
   listsContains,
+  listsNumbers,
 ]);
